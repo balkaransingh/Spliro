b3d801a0df889d6bcc092b46143097dcc42d705a
devarya <user@qwerty.local>
2015-08-19 19:09:42 +0530

    Initial Commit

---
+++ Spliro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spliro
+//
+//  Created by Vikram Singh Charan on 19/08/15.
+//  Copyright (c) 2015 com.aryavrat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spliro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spliro
+//
+//  Created by Vikram Singh Charan on 19/08/15.
+//  Copyright (c) 2015 com.aryavrat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
